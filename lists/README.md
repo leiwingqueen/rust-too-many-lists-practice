c5e1af4e1ab76a65ec0529d02b0473c8432c11c5
liyongquan <496326100@qq.com>
2021-02-01 22:35:22 +0800

    继续学习

---
+++ lists/README.md
@@ -0,0 +1,7 @@
+## 学习资料
+
+rust-too-many-lists的内容确实可以，用类似踩坑的方式带你一步步走。
+
+[中文翻译](https://weathfold.gitbooks.io/rust-too-many-lists-zhcn/content/)
+
+[英文原文](https://rust-unofficial.github.io/too-many-lists/index.html)
--- lists/src/first.rs
+++ lists/src/first.rs
@@ -27,9 +27,25 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Node {
             elem: elem,
+            //The only thing you can't do with an &mut is move the value out with no replacement
+            //这样会导致head的所有权发生变化，head指针的生命周期结束
+            //next:self.head,
             next: mem::replace(&mut self.head, Link::Empty),
         };
         self.head = Link::More(Box::new(new_node));
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        let head = mem::replace(&mut self.head, Link::Empty);
+        match head {
+            Link::Empty => {
+                None
+            }
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
     }
 }
 
